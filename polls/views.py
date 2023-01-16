8384fa856ccde33e87846fd8dc712e8494a03959
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-01-16 18:33:29 -0300

    criando templates html

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Olá esse é o meu site')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def results(request, question_id):
+    question = Question(pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    return HttpResponse('Você está voltando na questão de número %s' % question_id)
