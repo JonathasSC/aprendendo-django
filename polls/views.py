7d7bcc57752dd957a4004eadea1702d6120646d3
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-01-16 19:17:09 -0300

    fazendo o template html

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Question
+
+
 
 
 # Create your views here.
@@ -17,4 +20,15 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse('Você está voltando na questão de número %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except KeyError:
+        return render(request, 'polls/vote.html', {
+            "question": question,
+            "error_message": "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
